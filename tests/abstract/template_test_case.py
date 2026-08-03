e15e1db06bed696d85ddde66194763210d27627f
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-02 19:37:09 -0700

    [Copyright] ci: Use the seven-line header form in every Python file.

--- tests/abstract/template_test_case.py
+++ tests/abstract/template_test_case.py
@@ -1,8 +1,10 @@
+#
 # This file is part of the Project Template package.
 #
 # Copyright (c) 2016-present Melech Mizrachi
 #
 # Released under the MIT License. See LICENSE.md for details.
+#
 
 
 class TemplateTestCase:
